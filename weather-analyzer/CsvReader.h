1408ffec1d71f9279637b3cb6e467e96926dec3b
Martin Wickett <martintwickett@gmail.com>
2024-12-09 18:23:53 -0500

    Initial Commit

---
+++ weather-analyzer/CsvReader.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <vector>
+
+
+class CsvReader
+{
+public:
+	CsvReader();
+
+	/*static std::vector<OrderBookEntry> readcsv(std::string csvFile);
+	static std::vector<std::string> tokenize(std::string csvLine, char delimiter);
+	static OrderBookEntry stringsToOBE(std::string priceString,
+		std::string amountString,
+		std::string timestamp,
+		std::string product,
+		OrderBookType type);
+
+private:
+	static OrderBookEntry stringsToOBE(std::vector<std::string> tokens);*/
+};
+
