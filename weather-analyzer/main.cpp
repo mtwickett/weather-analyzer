5786bd5d34e4047955d98f61c82296c9c38ef938
Martin Wickett <martintwickett@gmail.com>
2024-12-11 22:09:05 -0500

    Built path with exception handled

--- weather-analyzer/main.cpp
+++ weather-analyzer/main.cpp
@@ -1,4 +1,24 @@
+#include <iostream>
+#include <fstream>
+#include <filesystem>
+#include <string>
+
+
 int main()
 {
+	std::filesystem::path rootPath = std::filesystem::current_path();
+	std::filesystem::path filePath = rootPath / "data" / "utc_timestamp,AT_temperature,BE_tem.txt";
+
+    try {
+        if (!std::filesystem::exists(filePath)) 
+        {
+            throw std::runtime_error("Error: Path does not exist: " + filePath.string());
+        }
+    }
+    catch (const std::exception& e) 
+    {
+        std::cerr << e.what() << std::endl;
+    }
+
 	return 0;
 }
